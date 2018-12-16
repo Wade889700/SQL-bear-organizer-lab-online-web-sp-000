51b92d1ac7f7c40a8d2b3c9159e770c1cd81bdf9
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 01:04:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Mr. Chocolate', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Rowdy', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Tabitha', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Sergeant Brown', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Melissa', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Grinch', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Wendy', 3, 'Scottish Fold');
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES (NULL, 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Mr. Chocolate', 2, 'Male');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Rowdy', 3, 'Female');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Tabitha', 4, 'Male');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Sergeant Brown', 5, 'Male');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Melissa', 6, 'Female');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Grinch', 7, 'Female');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Wendy', 8, 'Female');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES (NULL, 9, 'Male');
