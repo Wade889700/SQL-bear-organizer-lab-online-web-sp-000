a791e2fa05f374ad6157de829a8dc964f491aa4b
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 01:01:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,8 @@
-
-INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Mr. Chocolate', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Rowdy', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Tabitha', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Sergeant Brown', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Melissa', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Grinch', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES ('Wendy', 3, 'Scottish Fold');
+INSERT INTO bears (id, name, age, gender, color, temperament,alive ) VALUES (,NULL, 3, 'Scottish Fold');
